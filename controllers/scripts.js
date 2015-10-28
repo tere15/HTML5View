8b66d409874a4b6ba04f891e986a14e6795c0ffc
tere15 <kristiina.horneman@gmail.com>
2015-10-28 13:10:20 +0200

    Version 1.3

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,7 +1,10 @@
+
+"use strict";
 console.log("Here we go!!");
 
 //Wait document ready event, anonyymifunktion käyttö
 $(document).ready(function(){ 
+    // ready käynnistyy, kun selain saavuttaa päättävän body-tagin
     
     console.log("jquery onload triggered"); 
     
@@ -15,6 +18,42 @@ $(document).ready(function(){
     $(".about").html("<b>New text</b>");
     $("[data-dummy]").html("<p>Hello World</p>");
     
+    var setting = { 
+        // objekti tehään usein kuitenkin inline
+        method:"GET",
+        url:"http://localhost:28017/oma/person/",
+        dataType:"jsonp",
+        jsonp:"jsonp"
+    }
+    
+    $.ajax(setting).done(function(data){ 
+        //kun serveriltä vastaus, kutsu done-funktiota
+        console.log(data);
+        for(var i=0; i < data.rows.length; i++){
+            
+            var html = "<tr>" +
+                    "<td>" + data.rows[i].name + "</td>" +
+                    "<td>" + data.rows[i].address + "</td>" +
+                    "<td>" + data.rows[i].age + "</td>" +                
+                    "</tr>";
+            
+            $(html).appendTo("tbody");
+
+        }
+        
+        /*for(var i=0; i < data.rows.length; i++){
+            
+            var html =
+                    "<li>" + data.rows[i].name + "</li>" +
+                    "<li>" + data.rows[i].address + "</li>" +
+                    "<li>" + data.rows[i].age + "</li>"              
+                    ;
+            
+            $(html).appendTo("tbody");
+
+        } */       
+    });
+    
 });
 
 /*
@@ -23,17 +62,17 @@ $(document).ready(domReady);
 function domReady() { //nimellisen funktion käyttö
 }
 */
-
+/*
 var test = 123;
 test = "error"
 
-window.onload = function(event){    /* anonyymi funktio eli funktio, jolla ei nimeä */
+window.onload = function(event){     anonyymi funktio eli funktio, jolla ei nimeä 
     console.log(event);
     para1.innerHTML = "Changed from JS";
     para1.style.backgroundColor = "yellow";
 }
 
-window.onload = domReady; /*callback funktio*/
+window.onload = domReady; callback funktio
 
 function domReady(event){
     return 2;
@@ -45,3 +84,4 @@ function someFunction(nimi){
 
 someFunction();
 someFunction("Terhi");
+*/
